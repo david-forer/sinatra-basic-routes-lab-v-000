1d548970c5b93e29f15390d214da27941f392a6f
David Forer <david.j.forer@gmail.com>
2019-08-25 09:19:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
- get '/name' do
+ 
+  get '/name' do
    "My name is David Forer"
+  end
+ 
+  get '/hometown' do
+   "My hometown is La Crosse"
+  end
+  
+  get '/favorite-song' do
+   "My favorite sone is Bohemian Rhapsody"
+  end
   
 end
